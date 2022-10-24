385eac6d04cff3617afb4fd7dc8b64c8cb7a09b4
Ovidiu Lucian Cristina <63011880+ovidiu127@users.noreply.github.com>
2022-10-24 20:46:03 +0300

    Add files via upload

---
+++ spital.py
@@ -0,0 +1,26 @@
+class medic:
+    def __init__(self,nume,prenume,post):
+        self.nume=nume
+        self.prenume=prenume
+        self.post=post
+    def modifNume(self,nume):
+        self.nume=nume;
+        
+class spital:
+    def __init__(self,nume,numar_medici,specializare,medici):
+        self.nume=nume
+        self.numar_medici=numar_medici
+        self.specializare=specializare
+        self.medici=medici
+    def afisMedici(self):
+        for m in medici:
+            print(m.nume+" "+m.prenume)
+        
+medici=[medic("Popa","Gigi","chirurg"),medic("Coca","Nelu","ginecolog")]
+
+s=spital("SPJT",2,"chirurgie",medici)
+
+print(s.nume)
+s.afisMedici();
+s.medici[1].modifNume("Pretorian")
+s.afisMedici()
